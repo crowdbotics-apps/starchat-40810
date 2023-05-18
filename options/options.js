04803840662fc5e1eb07f2d9757cdbff60748aa1
Crowdbotics <team@crowdbotics.com>
2023-05-18 03:06:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "starchat-40810",
+  url: "https://starchat-40810.botics.co",
+  api: "https://starchat-40810.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
